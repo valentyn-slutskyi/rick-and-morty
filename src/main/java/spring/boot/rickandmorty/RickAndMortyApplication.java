c2045852164c86500cb56ff39f76c972cd86b5a5
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-11-23 09:26:44 +0200

    first commit

---
+++ src/main/java/spring/boot/rickandmorty/RickAndMortyApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/rickandmorty/RickAndMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
